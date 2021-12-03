1e985d418eb48da4aee846056d11f354df0f820d
Shanthanu Bhardwaj <xanth@google.com>
2021-12-03 19:44:01 +0000

    Part 2

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -12,7 +12,19 @@ fn main() {
     let number_of_increases = numbers.tuple_windows()
       .filter(|(prev,next)| next > prev)
       .count();
-    println!("Answer = {}", number_of_increases);
+    println!("Answer to part 1 = {}", number_of_increases);
+
+    // Part 2
+    let numbers= read_lines("./inputs/input1")
+    .expect("File read error")
+    .map(|s|s.expect("io error").parse::<i32>().expect("string parse error"));
+
+    let number_of_increases = numbers.tuple_windows()
+      .map(|(x,y,z)| x+y+z)
+      .tuple_windows()
+      .filter(|(prev,next)| next > prev)
+      .count();
+    println!("Answer to part 2 = {}", number_of_increases);
 }
 
 // The output is wrapped in a Result to allow matching on errors
